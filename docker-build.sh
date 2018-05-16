564067dbf490ebaf77a35ac741a6dde0648d7938
sujian <sujiansoft@163.com>
2018-05-16 16:26:00 +0800

    123

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,4 @@ docker rm -fv aaa
 docker build -t "registry.cn-beijing.aliyuncs.com/tigers/gitlabci:1.8.3" .
 docker push "registry.cn-beijing.aliyuncs.com/tigers/gitlabci:1.8.3"
 
-#docker run --name=aaa -d docker-registry.hexun.com/docker/gitlabci:1.8.3
+docker run --name=aaa -d registry.cn-beijing.aliyuncs.com/tigers/gitlabci:1.8.3
